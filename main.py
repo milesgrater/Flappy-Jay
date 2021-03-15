660a8db828137d60ffc8acc0bbb07f6bf2bdf5ec
Miles Grater <milesgrater@gmail.com>
2021-03-14 20:27:54 -0400

    Added Scrolling Clouds

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 from kivy.uix.image import Image
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivy.clock import Clock
 
 
-class StartScreen(Screen):
-    pass
+class Background(Widget):
+    cloud_texture = ObjectProperty(None)
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
+        # Gets texture version of cloud image
+        self.cloud_texture = Image(source = 'cloud.png').texture
 
+        # Allows clouds to repeat through out the sky
+        self.cloud_texture.wrap = 'repeat'
 
-class RootScreen(ScreenManager):
+        # Determines how big the cloud size is before being repeated
+        self.cloud_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
+
+    def scroll_textures(self, elasped_time):
+        # Updates the position of the cloud, and the modulo so it can be wrapped
+        self.cloud_texture.uvpos = ( (self.cloud_texture.uvpos[0] + elasped_time) % Window.width ,self.cloud_texture.uvpos[1])
+
+        # Draws the texture over again
+        texture = self.property('cloud_texture')
+        texture.dispatch(self)
+
     pass
 
 
 class FlappyApp(App):
-    def build(self):
-        return RootScreen()
+    def on_start(self):
+        # Acts as a frame rate 
+        Clock.schedule_interval(self.root.ids.background.scroll_textures, 1/60.)
+        
+    pass
 
-
-if __name__ == "__main__":
-    FlappyApp().run()
+    
+FlappyApp().run()
