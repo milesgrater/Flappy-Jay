e40932401cc97bf9ae7a634c1cfcc2f23c1e5fa0
Adamwolfe31 <adam_wolfe@comcast.net>
2021-03-18 20:27:18 -0400

    Added connectDB() and updateScore() functions

--- database_code.py
+++ database_code.py
@@ -19,27 +19,77 @@ def addScore(username, user_score):
                "(username, score) "
                "VALUES (%s, %s)")
 
-    #create generic test user to insert
+    #user input data
     data_score = (username, user_score)
 
-    #try/catch to insert user and score, prints error if username is already in use (username is primary key in table)
+    #try/catch to insert user and score, prints error if username is already in use (username is primary key in table) and allows for overwriting of existing score
     try:
         cursor.execute(add_score, data_score)
         connection.commit()
         print("\nUsername & score successfully added!\n")
+        ans = "yes"
     except:
         print("\nError: username already in use!\n")
+        print("\nWould you like to overwrite past score for player: ", username, "? (yes/no)", sep = '')
+        ans = input()
+
+        valid = 1
+        while valid != 0:
+            if ans.lower() == "yes":
+                updateScore(username, user_score)
+                valid = 0
+            elif ans.lower() == "no":
+                newName = input("\nEnter a new player name: ")
+                addScore(newName, user_score)
+                valid = 0
+            else:
+                print("Error: invalid input")
+                valid = 1
+        
+def updateScore(username, user_score):
+    """
+    Updates user game score for already created users
+    """
+    #sql statement to update leaderboard table at specified username
+    update_score = ("UPDATE leaderboard "
+                    "SET score = %s "
+                    "WHERE username = %s")
+
+    #user input data
+    data_score = (user_score, username)
+    
+    #try/catch to update user score if they want to overwrite an old user score (username is already in table but want to update that score)
+    try:
+        cursor.execute(update_score, data_score)
+        connection.commit()
+        print("\nUser score successfully updated!\n")
+    except:
+        print("\nError: user score not updated!\n")
+
+def connectDB(username, passwd, h, db):
+    """
+    Connects game client to the database for leaderboard access
+    """
+    return mysql.connector.connect(user=username, password=passwd, host=h, database=db)
 
 
 #creates connection to database for Flappy Jay (replace user, password, host, and database once finalized server is up and running)
-connection = mysql.connector.connect(user='flappy_jay', password='software', host='127.0.0.1', database='flappy_jay')
+usr = 'flappy_jay'
+pwd = 'software'
+h = '127.0.0.1'
+db = 'flappy_jay'
+connection = connectDB(usr, pwd, h, db)
 
 #creates cursor 
 cursor = connection.cursor()
 
-#set generic test user values for inserting into database
-username = 'Test'
-score = 100
+#prompt user to enter player name after finishing game
+username = input("Enter player name: ")
+
+#*************************************************************************
+#eventually change this score to get score from game environment directly
+#*************************************************************************
+score = 150
 
 #add user and score to leaderboard
 addScore(username, score)
