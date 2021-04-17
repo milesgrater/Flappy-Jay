3dcc3302efc9aefa3a26a764bce36796e815a63a
Adamwolfe31 <adam_wolfe@comcast.net>
2021-04-17 14:28:40 -0400

    Edit: passing scores by int value

--- database_code.py
+++ database_code.py
@@ -20,7 +20,7 @@ def addScore(username, user_score):
                "VALUES (%s, %s)")
 
     #user input data
-    data_score = (username, user_score)
+    data_score = (username, int(user_score))
 
     #try/catch to insert user and score, prints error if username is already in use (username is primary key in table) and allows for overwriting of existing score
     try:
@@ -56,7 +56,7 @@ def updateScore(username, user_score):
                     "WHERE username = %s")
 
     #user input data
-    data_score = (user_score, username)
+    data_score = (int(user_score), username)
     
     #try/catch to update user score if they want to overwrite an old user score (username is already in table but want to update that score)
     try:
@@ -97,7 +97,7 @@ username = input("Enter player name: ")
 #*************************************************************************
 #eventually change this score to get score from game environment directly
 #*************************************************************************
-score = 150
+score = 75
 
 #add user and score to leaderboard
 addScore(username, score)
