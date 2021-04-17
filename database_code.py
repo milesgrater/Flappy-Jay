526e9db194e42aa9b68531e8cc1fce45e7d21fe3
Miles Grater <58203137+milesgrater@users.noreply.github.com>
2021-04-17 15:33:53 -0400

    Update database_code.py

--- database_code.py
+++ database_code.py
@@ -76,8 +76,6 @@ def connectDB(username, passwd, h, db):
 
 
 #creates connection to database for Flappy Jay (replace user, password, host, and database once finalized server is up and running)
-<<<<<<< HEAD
-=======
 #usr = 'flappy_jay'
 #pwd = 'software'
 #h = '127.0.0.1'
@@ -85,7 +83,6 @@ def connectDB(username, passwd, h, db):
 #connection = connectDB(usr, pwd, h, db)
 
 #connects to school server
->>>>>>> fad9db9700345439409d1f05f7c8b9ad726c8255
 usr = 'wolfea'
 pwd = 'wolfea'
 h = '172.16.86.208'
@@ -98,7 +95,6 @@ cursor = connection.cursor()
 # #prompt user to enter player name after finishing game
 # username = input("Enter player name: ")
 
-<<<<<<< HEAD
 #*************************************************************************
 #eventually change this score to get score from game environment directly
 #*************************************************************************
@@ -108,7 +104,6 @@ cursor = connection.cursor()
 # #eventually change this score to get score from game environment directly
 # #*************************************************************************
 # score = 75
->>>>>>> fad9db9700345439409d1f05f7c8b9ad726c8255
 
 # #add user and score to leaderboard
 # addScore(username, score)
@@ -116,8 +111,6 @@ cursor = connection.cursor()
 # #can use to reset leaderboard
 # #clearLeaderboard()
 
-
-<<<<<<< HEAD
 #close cursor and database connection
 # cursor.close()
 # connection.close()
@@ -125,4 +118,4 @@ cursor = connection.cursor()
 # #close cursor and database connection
 #cursor.close()
 #connection.close()
->>>>>>> fad9db9700345439409d1f05f7c8b9ad726c8255
+
